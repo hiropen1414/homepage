edb2b75489e20df1ac1bb30e5ea263325afa80b4
hiropen <20s3056@stu.meisei-u.ac.jp>
2022-12-29 23:13:50 +0900

    [fix]: リログするとエラーになる問題を修正

--- src/Commons/constants/path.ts
+++ src/Commons/constants/path.ts
@@ -2,6 +2,7 @@ export const GAME_PATH = '/game';
 export const MEDIA_PATH = '/media';
 export const WEB_PATH = '/web';
 export const ELECT_PATH = '/elect';
+export const ERROR_PATH = '/error';
 export const INQUIRY_PATH = '/inquiry';
 
 export const HOME_URL = process.env.PUBLIC_URL;
--- src/Pages/App.tsx
+++ src/Pages/App.tsx
@@ -2,7 +2,7 @@ import Top from './Top';
 import { Game } from './Game';
 import { Error } from './Error';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { GAME_PATH, HOME_URL } from '../Commons/constants/path';
+import { ERROR_PATH, GAME_PATH, HOME_URL } from '../Commons/constants/path';
 import { NOT_FOUND_CODE } from '../Commons/constants/errorcode';
 import { NOT_FOUND } from '../Commons/constants/text';
 
@@ -12,6 +12,7 @@ const App = () => {
       <Routes>
         <Route path={HOME_URL} element={<Top />} />
         <Route path={GAME_PATH} element={<Game />} />
+        <Route path={ERROR_PATH} element={<Error state={{ errorCode: NOT_FOUND_CODE, errorMessage: NOT_FOUND }} />} />
         <Route path='*' element={<Error state={{ errorCode: NOT_FOUND_CODE, errorMessage: NOT_FOUND }} />} />
       </Routes>
     </BrowserRouter>
