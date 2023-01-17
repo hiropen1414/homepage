9462e32b7035500ee9d7b1a49f72f73f8dfc6830
hiropen <hiropen1414@gmail.com>
2023-01-17 19:28:15 +0900

    [update]: mediaページの作成をしました。

--- src/Commons/Molecules/Serach/Serach.tsx
+++ src/Commons/Molecules/Serach/Serach.tsx
@@ -1,12 +1,13 @@
 import { Input } from 'antd';
 
-type Props={
-  holder?:string;
+type Props = {
+  holder?: string;
 }
 
-export const Search = (props:Props) => {
+export const Search = (props: Props) => {
   const { Search } = Input;
-  const onSearch = (value: string) => console.log(value);
+  // const onSearch = (value: string) => console.log(value);
+  const onSearch = () => { return null; };
   return (
     <Search placeholder={props.holder} onSearch={onSearch} style={{ width: 200 }} />
   );
--- src/Pages/App.tsx
+++ src/Pages/App.tsx
@@ -2,10 +2,11 @@ import Top from './Top/Top';
 import { Game } from './Game/Game';
 import { Error } from './Error';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { ERROR_PATH, GAME_PATH, HOME_URL } from '../Commons/constants/path';
+import { ERROR_PATH, GAME_PATH, HOME_URL, MEDIA_PATH } from '../Commons/constants/path';
 import { NOT_FOUND_CODE } from '../Commons/constants/errorcode';
 import { NOT_FOUND } from '../Commons/constants/text';
 import { ScrollToTop } from '../Commons/static/function/ScrollToTop';
+import { Media } from './Media/Media';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Routes>
           <Route path={HOME_URL} element={<Top />} />
           <Route path={GAME_PATH} element={<Game />} />
+          <Route path={MEDIA_PATH} element={<Media />} />
           <Route path={ERROR_PATH} element={<Error state={{ errorCode: NOT_FOUND_CODE, errorMessage: NOT_FOUND }} />} />
           <Route path='*' element={<Error state={{ errorCode: NOT_FOUND_CODE, errorMessage: NOT_FOUND }} />} />
         </Routes>
