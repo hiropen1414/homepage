73f9fc3d7c62c861457546ddd048dc96b888dfab
hiropen <hiropen1414@gmail.com>
2023-01-29 20:12:19 +0900

    [update]: よくある質問ページを作成しました

---
+++ src/Commons/Organisms/FAQAccordion.tsx
@@ -0,0 +1,102 @@
+import { CaretDownOutlined, CaretUpOutlined } from '@ant-design/icons';
+import { isEqual } from 'lodash';
+import { useState } from 'react';
+import { Image } from '../Atoms/Image/Image';
+import IconQuestion from '../static/svg/question-chat.svg';
+import IconAnswer from '../static/svg/check-circle.svg';
+import { AccordionType } from '../../Pages/Question/questionInfo';
+
+type Props = {
+  content: AccordionType[];
+}
+
+export const Accordion = (props: Props) => {
+  const styles = {
+    questionTitle: {
+      display: 'flex',
+      width: '100%',
+    },
+    icon: {
+      width: 48,
+      marginBottom: 16,
+    },
+    questionSentence: {
+      margin: '0px 8px',
+      fontSize: '1.75em',
+    },
+    questionBox: {
+      width: 1400,
+      maxWidth: '100%',
+      paddingLeft: 48,
+    },
+    openCheckButton: {
+      fontSize: '2.5em',
+      color: 'rgb(255,120,120)',
+      marginTop: 4,
+    },
+    closeCheckButton: {
+      fontSize: '2.5em',
+      color: 'rgb(80,200,255)',
+      marginTop: 4,
+    },
+    qaBox: {
+      width: 'fit-content',
+    },
+    answerBox: {
+      display: 'flex'
+    },
+    answerContent: {
+      display: 'block',
+      fontSize: '1.5em',
+      marginLeft: 8,
+      marginTop: 8
+    },
+    bottomBorder: {
+      marginLeft: -48,
+      marginBottom: 32,
+      borderBottom: '1px dashed lightgray'
+    }
+  };
+
+  const [accordions, setAccordions] = useState<AccordionType[]>(props.content);
+  const toggleAccordion = (id: number) => {
+    setAccordions(
+      accordions.map((accordion, accordionId) => {
+        if (isEqual(accordionId, id)) {
+          return { ...accordion, isOpen: !accordion.isOpen };
+        }
+        return accordion;
+      })
+    );
+  };
+
+  return (
+    <div style={styles.questionBox}>
+      {accordions.map((accordion, accordionId) => (
+        <>
+          <div style={styles.qaBox} key={accordionId}>
+            <div style={styles.questionTitle} onClick={() => toggleAccordion(accordionId)}>
+              <Image styles={styles.icon} src={IconQuestion} preview={false} />
+              <p style={styles.questionSentence} >
+                {accordion.title}
+              </p>
+              {(accordion.isOpen)
+                ? <CaretDownOutlined style={styles.closeCheckButton} />
+                : <CaretUpOutlined style={styles.openCheckButton} />
+              }
+            </div>
+            {accordion.isOpen &&
+              <div style={styles.answerBox}>
+                <Image styles={styles.icon} src={IconAnswer} preview={false} />
+                <div style={styles.answerContent}>
+                  {accordion.content}
+                </div>
+              </div>
+            }
+          </div>
+          <div style={styles.bottomBorder}></div>
+        </>
+      ))}
+    </div>
+  );
+};
--- src/Pages/App.tsx
+++ src/Pages/App.tsx
@@ -2,11 +2,12 @@ import Top from './Top/Top';
 import { Game } from './Game/Game';
 import { Error } from './Error';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { ERROR_PATH, GAME_PATH, HOME_URL, MEDIA_PATH } from '../Commons/constants/path';
+import { ERROR_PATH, GAME_PATH, HOME_URL, MEDIA_PATH, QUESTION_PATH } from '../Commons/constants/path';
 import { NOT_FOUND_CODE } from '../Commons/constants/errorcode';
 import { NOT_FOUND } from '../Commons/constants/text';
 import { ScrollToTop } from '../Commons/static/function/ScrollToTop';
 import { Media } from './Media/Media';
+import { Question } from './Question/Question';
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
           <Route path={HOME_URL} element={<Top />} />
           <Route path={GAME_PATH} element={<Game />} />
           <Route path={MEDIA_PATH} element={<Media />} />
+          <Route path={QUESTION_PATH} element={<Question />} />
           <Route path={ERROR_PATH} element={<Error state={{ errorCode: NOT_FOUND_CODE, errorMessage: NOT_FOUND }} />} />
           <Route path='*' element={<Error state={{ errorCode: NOT_FOUND_CODE, errorMessage: NOT_FOUND }} />} />
         </Routes>
