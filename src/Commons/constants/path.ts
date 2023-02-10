71a760dd5ed23bb3c44957e5ed8846c9543d7a70
hiropen <hiropen1414@gmail.com>
2023-02-10 10:38:48 +0900

    path test

--- src/Commons/constants/path.ts
+++ src/Commons/constants/path.ts
@@ -1,13 +1,14 @@
-export const GAME_PATH = '/game';
-export const MEDIA_PATH = '/media';
-export const WEB_PATH = '/web';
-export const ELECT_PATH = '/elect';
-export const ERROR_PATH = '/error';
-export const QUESTION_PATH = '/question';
-export const INQUIRY_PATH = '/inquiry';
-export const LOGIN_PATH = '/login';
-export const CLUB_MEMBER_TOP_PATH = '/login/clubMemberTop';
-export const CLUB_MEMBER_TOP_CHILD = '/clubMemberTop';
-
 export const HOME_URL = process.env.PUBLIC_URL;
+
+export const GAME_PATH =  HOME_URL + '/game';
+export const MEDIA_PATH = HOME_URL +  '/media';
+export const WEB_PATH = HOME_URL +  '/web';
+export const ELECT_PATH = HOME_URL +  '/elect';
+export const ERROR_PATH = HOME_URL +  '/error';
+export const QUESTION_PATH = HOME_URL +  '/question';
+export const INQUIRY_PATH = HOME_URL +  '/inquiry';
+export const LOGIN_PATH = HOME_URL +  '/login';
+export const CLUB_MEMBER_TOP_PATH = HOME_URL +  '/login/clubMemberTop';
+export const CLUB_MEMBER_TOP_CHILD = HOME_URL +  '/clubMemberTop';
+
 
